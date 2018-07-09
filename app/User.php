44bbef4cf1068a2ca5d164dd2f63acf6aa82edec
Stanislav Khatko <s.a.hatko@gmail.com>
2018-07-09 15:58:04 +0200

    Added passport

--- app/User.php
+++ app/User.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable {
-	use Notifiable;
+	use Notifiable, HasApiTokens;
 
 	/**
 	 * The attributes that are mass assignable.
