49a9d6e8ecd9bf61d2d26cfc935028cf062ff3e6
Stanislav Khatko <s.a.hatko@gmail.com>
2018-07-17 15:39:00 +0300

    Fix

--- resources/views/artist/index.blade.php
+++ resources/views/artist/index.blade.php
@@ -16,30 +16,33 @@
 
                 <div class="artists">
 
-                    @foreach($artists as $artist)
+                    @if(count($artists))
+                        @foreach($artists as $artist)
 
-                        <el-card class="artist">
+                            <el-card class="artist">
 
-                            <a href="{{ route('artist', $artist->id) }}" class="artist-photo">
-                                <img src="{{ $artist->photo->name }}" alt="">
-                            </a>
+                                <a href="{{ route('artist', $artist->id) }}" class="artist-photo">
+                                    <img src="{{ $artist->photo->name }}" alt="">
+                                </a>
 
-                            <a href="{{ route('artist', $artist->id) }}" class="artist-name" style="display: block;">
-                                {{ $artist->name }}
-                            </a>
+                                <a href="{{ route('artist', $artist->id) }}" class="artist-name"
+                                   style="display: block;">
+                                    {{ $artist->name }}
+                                </a>
 
-                            <div class="artist-panel">
-                                <a href="#" class="artist-favorite"><span class="el-icon-star-off"></span></a>
-                            </div>
+                                <div class="artist-panel">
+                                    <a href="#" class="artist-favorite"><span class="el-icon-star-off"></span></a>
+                                </div>
 
-                            <div class="h4">{{ $artist->user['name'] }}</div>
+                                <div class="h4">{{ $artist->user['name'] }}</div>
 
-                            <div class="h5">{{ $artist->country['country_name'] }}</div>
+                                <div class="h5">{{ $artist->country['country_name'] }}</div>
 
 
-                        </el-card>
+                            </el-card>
 
-                    @endforeach
+                        @endforeach
+                    @endif
 
                 </div>
             </main>
