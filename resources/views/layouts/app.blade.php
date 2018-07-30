84185d5ee7d708d5257cd33bc2785f75676f179b
Stanislav Khatko <s.a.hatko@gmail.com>
2018-07-30 14:33:19 +0300

    Added promo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,7 +153,7 @@
                                     </el-dropdown-item>
 
                                     <el-dropdown-item>
-                                        <i class="icon-user"></i>
+                                        <i class="icon-user-outline"></i>
                                         <a href="{{ route('admin.users') }}">Users</a>
                                     </el-dropdown-item>
 
@@ -221,7 +221,8 @@
                     </div>
                 @endif
 
-                <a href="{{ route('dashboard.favorites') }}" class="app-header-star" v-if="$store.state.favouritesCount">
+                <a href="{{ route('dashboard.favorites') }}" class="app-header-star"
+                   v-if="$store.state.favouritesCount">
                     <i class="el-icon-star-off"></i><sup>@{{ $store.state.favouritesCount }}</sup>
                 </a>
 
@@ -277,6 +278,94 @@
 
             <div class="app-footer--top">
 
+                <div class="app--wrapper">
+
+                    <div class="app-footer-promos">
+
+                        <div class="promo">
+                            <div class="promo--top">
+                                <div class="promo-image">
+                                    <i class="icon-wallet"></i>
+                                </div>
+
+                                <div class="h2">
+                                    Secured Payment
+                                </div>
+
+                                <div class="promo-description">
+                                    <div>Via</div>
+                                    <div class="icons">
+                                        <i class="icon-mastercard"></i>
+                                        <i class="icon-visa"></i>
+                                        <i class="icon-cc-paypal"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <a href="{{ route('page', 'warranty')}}" class="promo-link">Learn more</a>
+
+                        </div>
+
+
+                        <div class="promo">
+                            <div class="promo--top">
+                                <div class="promo-image">
+                                    <i class="icon-truck"></i>
+                                </div>
+
+                                <div class="h2">
+                                    Fast Delivery
+                                </div>
+
+                                <div class="promo-description">
+                                    We use DHL as a courier. Delivery time is dependent on recipient country
+                                </div>
+                            </div>
+
+                            <a href="{{ route('page', 'freight')}}" class="promo-link">Learn more</a>
+                        </div>
+
+                        <div class="promo">
+                            <div class="promo--top">
+                                <div class="promo-image">
+                                    <i class="icon-dropbox"></i>
+                                </div>
+
+                                <div class="h2">
+                                    Free Delivery
+                                </div>
+
+                                <div class="promo-description">
+                                    We deliver your new work of art freely
+                                </div>
+                            </div>
+
+                            <a href="{{ route('page', 'freight')}}" class="promo-link">Learn more</a>
+                        </div>
+
+                        <div class="promo">
+                            <div class="promo--top">
+                                <div class="promo-image">
+                                    <i class="icon-megaphone"></i>
+                                </div>
+
+                                <div class="h2">
+                                    Customer Support
+                                </div>
+
+                                <div class="promo-description">
+                                    +45 XXXX XXXX
+                                    sales@beartespace.com
+                                </div>
+                            </div>
+
+                            <a href="{{ route('contact-form') }}" class="promo-link">Learn more</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
@@ -319,8 +408,7 @@
                                 <a href="{{ route('page', 'warranty')}}">Warranty</a>
                                 <a href="{{ route('page', 'taxes')}}">Taxes</a>
                                 <a href="{{ route('page', 'freight')}}">Freight</a>
-                                <a href="{{ route('page', 'cookies-and-privacy')}}">Cookies and Privacy
-                                    Regulation</a>
+                                <a href="{{ route('page', 'cookies-and-privacy')}}">Cookies and Privacy Regulation</a>
                                 <a href="{{ route('contact-form') }}">@lang('portal.contact')</a>
                             </div>
 
