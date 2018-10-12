670b2631205e582ad986237cc232e436c289d1eb
Stanislav Khatko <s.a.hatko@gmail.com>
2018-10-12 18:15:54 +0900

    fix

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define( \App\Article::class, function ( Faker $faker ) {
 	return [
 		'name'   => $faker->sentence,
-		'content' => $faker->sentence,
+		'content' => $faker->paragraphs(10, true),
 		'slug'    => $faker->slug,
 		'active'  => $faker->boolean
 	];
