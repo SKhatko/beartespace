5048595247f21fc10f35d2c99785178f4c3a623c
Stanislav Khatko <s.a.hatko@gmail.com>
2018-10-29 17:00:04 +0800

    Test

--- resources/views/layouts/header/bottom.blade.php
+++ resources/views/layouts/header/bottom.blade.php
@@ -2,15 +2,31 @@
 
     <nav class="app-header-nav hidden-xs-only">
 
+
+        <el-popover
+                placement="bottom"
+                {{--width="400"--}}
+                trigger="hover">
+
+            @foreach(trans('artwork-category') as $artworkCategory)
+                <el-dropdown-item>
+                    <a href="/artwork?artwork-category={{ $artworkCategory }}"
+                       style="display: block;margin:10px 0">{{ $artworkCategory }}</a>
+                </el-dropdown-item>
+            @endforeach
+
+            <a href="{{ route('artworks') }}" slot="reference">Artworks</a>
+        </el-popover>
+
+
         <a href="{{ route('people') }}">@lang('portal.artists')</a>
-        <a href="{{ route('artworks') }}">@lang('portal.artworks')</a>
+        {{--<a href="{{ route('artworks') }}">@lang('portal.artworks')</a>--}}
         {{--<a href="{{ route('auctions') }}">on-line auctions</a>--}}
         <a href="{{ route('articles') }}">Articles</a>
 
     </nav>
 
     <nav class="app-header-nav hidden-sm-and-up" v-if="showMainMenu">
-
 
         <a href="{{ route('people') }}">@lang('portal.artists')</a>
         <a href="{{ route('artworks') }}">@lang('portal.artworks')</a>
