eeb8f4a609c6611201469c8cd7c3a283f92f3de4
Stanislav Khatko <s.a.hatko@gmail.com>
2018-11-04 20:25:58 +0800

    Added shoppingcart

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -158,13 +158,14 @@ const app = new Vue({
 
 
 
-        // axios.get('/api/profile')
-        //     .then(response => {
-        //         // console.log('profile', response.data);
-        //     })
-        //     .catch(error => {
-        //         console.log(error.response);
-        //     })
+        axios.get('/api/profile')
+            .then(response => {
+                console.log('yea!!!');
+                // console.log('profile', response.data);
+            })
+            .catch(error => {
+                console.log(error.response);
+            })
     },
     methods: {}
 });
